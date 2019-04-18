6ab0032a79885154314770318f9b3c9df898da9d
Frank Richard Semakula <franksemakula@gmail.com>
2019-04-18 19:51:17 +0200

    Changes to express

--- pwa/express/index.js
+++ pwa/express/index.js
@@ -1,7 +1,58 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const router = express.Router();
+const mongoose = require('mongoose');
+const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('HI ROOT!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Telling the console that the server is running on port 8080
+app.listen(port, () => console.log(`SurveyFive listening on port ${port}!`));
+
+/***** MIDDLEWARE *****/
+/***** MIDDLEWARE *****/
+
+const timestamp = new Date().toLocaleTimeString();
+
+app.use((req, res, next) => {});
+
+/***** MIDDLEWARE *****/
+/***** DATA *****/
+/***** DATA *****/
+
+// Establishes the connection to the database
+const mongoURI =
+  'mongodb+srv://admin:admin@hoima-dsbni.mongodb.net/questionnaire?retryWrites=true';
+// const mongoURI = process.env.REACT_APP_MONGO;
+
+const db = mongoURI;
+
+// Provides info on connection status
+// MongoDB changed some things which requires the "useNewUrlParser: true" added
+// https://mongoosejs.com/docs/deprecations.html
+mongoose
+  .connect(db, { dbName: 'questionnaire', useNewUrlParser: true })
+  .then(() => console.log(`${'\n'}🕒  ${timestamp} 🕒 ${'\n'}✅ ✅ ✅  WERE LIVE! MongoDB SUCCESSFULLY CONNECTED ${'\n'}`))
+  .catch(err => console.error(`${'\n'}❌ ❌ ❌  CONNECTION ERROR: `, err));
+
+// db.on('error', console.error.bind(console, 'CONNECTION ERROR: '));
+// db.once('open', () => {
+//   console.log('WERE LIVE');
+// });
+
+const questionnaireSchema = new mongoose.Schema({
+  title: String,
+  type: Number,
+  required: Boolean,
+});
+
+// Questionnaire Model
+const Questions = mongoose.model('question', questionnaireSchema, 'questions');
+
+/***** DATA *****/
+/***** ROUTES *****/
+/***** ROUTES *****/
+
+router.get('/Welcome', (req, res) => {
+  res.json(Questions.find().then(Questions => res.json(Questions)));
+});
