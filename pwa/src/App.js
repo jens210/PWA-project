92f8310ff9f91e0c06ab741d719b3f87a5f839ee
Jens <Jensvk94@gmail.com>
2019-04-25 13:24:07 +0200

    addQuestionnaire method + small fixes in Questionnaire component

--- pwa/src/App.js
+++ pwa/src/App.js
@@ -9,34 +9,67 @@ import NotFound from './pages/NotFound';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      answers: []
+    };
     this.onChange = this.onChange.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.addQuestionaire = this.addQuestionaire.bind(this);
   }
 
   API_URL = 'http://localhost:8080/answers';
-  // API_URL = 'http://localhost:8080/questions';
+
+  addQuestionaire(nickname, q1, q2, q3, q4, q5, q6, q7, q8, q9) {
+    // Posting JSON to API
+    fetch('API_URL', {
+      method: 'POST',
+      body: JSON.stringify({
+        nickname: nickname,
+        q1: q1,
+        q2: q2,
+        q3: q3,
+        q4: q4,
+        q5: q5,
+        q6: q6,
+        q7: q7,
+        q8: q8,
+        q9: q9
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log("Result of posting a new Questionaire:");
+        console.log(json);
+      });
+  }
 
   // TODO
   handleInput(event) {
     event.preventDefault();
     console.log('Go to questionaire component and save the name');
-    /*
     this.addQuestionaire(
-    );*/
+      this.state.nickname,
+      this.state.q1,
+      this.state.q2,
+      this.state.q3,
+      this.state.q4,
+      this.state.q5,
+      this.state.q6,
+      this.state.q7,
+      this.state.q8,
+      this.state.q9
+    );
   }
 
   // TODO
   onChange(event) {
     //console.log(event.target.value);
-    /* this.setState({
-       //[event.target.name]: event.target.value
-     });*/
-  }
-
-  // TODO
-  addQuestionaire() {
-    // Posting JSON to API
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render() {
@@ -57,7 +90,11 @@ class App extends Component {
             />
             {/*TODO route for question component */}
             {/* ROUTE QUESTIONS */}
-            <Route exact path={'/Questionnaire'} render={props => <Questionnaire {...props} /> } />
+            <Route exact path={'/Questionnaire'} render={props => <Questionnaire {...props}
+              addQuestionaire={this.addQuestionaire}
+              onChange={this.onChange}
+              handleInput={this.handleInput}
+            />} />
 
             {/* ROUTE SUCCESS */}
             <Route exact path={'/Succes'} render={props => <Succes />} />
