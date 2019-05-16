2589f12021ff70e5b02b5b2cc745c6cff5fb339b
pepperpie <bushatka@gmail.com>
2019-05-16 09:45:51 +0200

    all components with the same style, values of radio changed to yes/no, summary on success

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -3,30 +3,62 @@ import React, { Component } from 'react';
 class Success extends Component {
   render() {
     return (
-      <div className="container success">
-      <br />
-      <br />
-      <br />
-
-        <h1>
-          <span role="img" aria-label="success">
-            🙌
-          </span>
-          Success
-          <span role="img" aria-label="success">
-            🙌
-          </span>
-        </h1>
-        <br />
-        <br />
-        <br />
-
-        <h2>
-          {this.props.answers.nickname}
-          <br />
-          Thank you for your donation
-        </h2>
-      </div>
+        <div className="success">
+          <div className="header">
+            <h1>
+            <span role="img" aria-label="success">
+              🙌
+            </span>
+              Success
+              <span role="img" aria-label="success">
+              🙌
+            </span>
+            </h1>
+            <p>Thank you for your candor {this.props.answers.nickname}. Now, we encourage you to finish your donation.</p>
+          </div>
+          <div className="review">
+            <p>Summary of your answers</p>
+            <div className="review_answer">
+              <p><span className="review_answer_number">1.</span> Number of hours since last release</p>
+              <p>{this.props.answers.q1}</p>
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">2.</span> Did you have a&nbsp;fever and/or did you visit a&nbsp;doctor?</p>
+              <p>{this.props.answers.q2}</p>
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">3.</span> Did you have an unprotected sex with a&nbsp;new sexual partner?</p>
+              <p>{this.props.answers.q3}</p>
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">4.</span> Did you have sex with a&nbsp;prostitute since the last donation?</p>
+              <p>{this.props.answers.q4}</p>
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">5.</span> Do you plan to travel in near future?</p>
+              <p>{this.props.answers.q5}</p>
+              {this.props.answers.q5_detail != null ? (<p>Details: {this.props.answers.q5_detail}</p>) : null}
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">6.</span> Have you recently been traveling outside the country, that you did not inform about?</p>
+              <p>{this.props.answers.q6}</p>
+              {this.props.answers.q6_detail != null ? (<p>Details: {this.props.answers.q6_detail}</p>) : null}
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">7.</span> Did you get a&nbsp;new piercing, tattoo, or similar?</p>
+              <p>{this.props.answers.q7}</p>
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">8.</span> Do you have a genital wart, vaginal epithelium or a&nbsp;wound on your genitals?</p>
+              <p>{this.props.answers.q8}</p>
+            </div>
+            <div className="review_answer">
+              <p><span className="review_answer_number">9.</span> Did you take any narcotics since the last donation?</p>
+              <p>{this.props.answers.q9}</p>
+              {this.props.answers.q9_detail != null ? (<p>Details: {this.props.answers.q9_detail}</p>) : null}
+            </div>
+          </div>
+        </div>
     );
   }
 }
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,44 +2,43 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Welcome extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { input: [] };
-  }
+        this.state = { success: false, input: [] };
+    }
 
-  render() {
-    return (
-      <form>
-        <h1 className="text-center"> Welcome </h1>
-        <h2 className="subtitle">
-          - We strongly encourage you to fill our this form before every visit
-        </h2>
-        <div className="form-group">
-          <input
-            name="nickname"
-            required
-            type="text"
-            className="form-control"
-            placeholder="Type your name"
-            onChange={this.props.onChange}
-          />
-        </div>
+    render() {
+        return (
+            <form>
+                <div className="welcome">
+                    <h1 className="title"> Welcome </h1>
+                    <p>We strongly encourage you to fill our this form before every visit</p>
+                    <input
+                        name="nickname"
+                        required
+                        type="text"
+                        className="question_input"
+                        placeholder="Type your name"
+                        onChange={this.props.onChange}
+                    />
 
-        <Link
-          className="button btn btn-info is-primary"
-          to={{
-            pathname: `/Questionnaire`,
-            state: { nickname: this.state.nickname },
-          }}
-          disabled={this.props.nickname.length < 1 ? true : false}
-        >
-          {' '}
-          Start questionnaire
-        </Link>
-      </form>
-    );
-  }
+                    <Link
+                        className="button btn btn-info is-primary"
+                        to={{
+                            pathname: `/Questionnaire`,
+                            state: { nickname: this.state.nickname },
+                        }}
+                        disabled={this.props.nickname.length < 1 ? true : false}
+                    >
+                        {' '}
+                        Start questionnaire
+                    </Link>
+                </div>
+
+            </form>
+        );
+    }
 }
 
 export default Welcome;
