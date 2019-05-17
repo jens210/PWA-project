2119a4b7b58e4ce346b62096b2c7419be59db791
Frank Richard Semakula <franksemakula@gmail.com>
2019-05-17 18:25:11 +0200

    Added disable to welcome component

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,44 +2,45 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Welcome extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { success: false, input: [] };
-    }
+    this.state = { success: false, input: [] };
+  }
 
-    render() {
-        return (
-            <form>
-                <div className="welcome">
-                    <h1 className="title"> Welcome </h1>
-                    <p>We strongly encourage you to fill our this form before every visit</p>
-                    <input
-                        name="nickname"
-                        data-msg-required="Write a name"
-                        required="required"
-                        type="text"
-                        className="question_input"
-                        placeholder="Type your name"
-                        onChange={this.props.onChange}
-                    />
+  render() {
+    return (
+      <form>
+        <div className="welcome">
+          <h1 className="title"> Welcome </h1>
+          <p>
+            We strongly encourage you to fill our this form before every visit
+          </p>
+          <input
+            name="nickname"
+            data-msg-required="Write a name"
+            required="required"
+            type="text"
+            className="question_input"
+            placeholder="Type your name"
+            onChange={this.props.onChange}
+          />
 
-                    <Link
-                        className="button btn btn-info is-primary"
-                        to={{
-                            pathname: `/Questionnaire`,
-                            state: { nickname: this.state.nickname },
-                        }}
-                        disabled={this.props.nickname.length < 1 ? true : false}
-                    >
-                        {' '}
-                        Start questionnaire
-                    </Link>
-                </div>
-
-            </form>
-        );
-    }
+          <Link
+            className={`button btn btn-info is-primary ${this.props.nickname.length < 1 ? 'disabled--link' : ''}`}
+            to={{
+              pathname: `/Questionnaire`,
+              state: { nickname: this.state.nickname },
+            }}
+            disabled={this.props.nickname.length < 1 ? true : false}
+          >
+            {' '}
+            Start questionnaire
+          </Link>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Welcome;
